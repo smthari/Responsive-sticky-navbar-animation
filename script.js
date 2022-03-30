99bf3b487398c1b5c768923123de265191e13af0
Sumit Harijan <smtharijan@gmail.com>
2022-03-30 21:21:45 +0530

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // Call scrollFunction on scroll 
-window.onscroll = function () {
-  scrollFunction();
-};
-function scrollFunction() {
-  document.getElementById("navbar").style.background = "#fff";
-}
 
 // functionality for toggle .nav-open class on click navToggle 
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
-
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-});
 
 // Auto close Navbar when click on link (responsive mode)
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
-  });
-});
-
